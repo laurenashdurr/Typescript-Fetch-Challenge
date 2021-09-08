ef0f4986ef9adc27c273a82f9184c5647ef70c17
Lauren Durr <laurenashdurr@gmail.com>
2021-09-08 17:18:26 -0400

    completed

--- src/common/OpenWeather.tsx
+++ src/common/OpenWeather.tsx
@@ -5,24 +5,39 @@ type WeatherProps = {
     lng: number
 }
 
-class OpenWeather extends Component <WeatherProps, {}> {
+type WeatherState = {
+    result: number
+}
+
+
+class OpenWeather extends Component <WeatherProps, WeatherState> {
     constructor(props: WeatherProps){
         super(props)
+
+        this.state = {
+            result: 0
+        }
     }
 
     async componentDidMount(){
-       let res = await fetch ("api.openweathermap.org/data/2.5/weather?lat="+ this.props.lat + "&lon=" + this.props.lng + "&appid=cf6181dc5bec9f976cc33d0907c33708")
+       let res = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lng}&appid=cf6181dc5bec9f976cc33d0907c33708`)
 
-    //    let json = await res.json()
+       let json = await res.json()
+       console.log(json)
 
-       console.log(res)
+       console.log(json.main.temp)
+
+       this.setState({
+           result: json.main.temp
+       })
        
     }
 
     render() {
         return (
             <div>
-                Open Weather Testing
+                <h1>Open Weather Testing</h1>
+              <p>{this.state.result} degrees kelvin</p>
             </div>
         )
     }
